ae40bf835ee5a1769410db26840110e5514e851b
Matheus Figueiredo <figueiredo.matheus06@gmail.com>
2023-10-26 21:18:52 -0300

    chore: better solution to two sum

--- two-sum/two-sum.go
+++ two-sum/two-sum.go
@@ -4,21 +4,33 @@ import "fmt"
 
 // CHALLENGE: https://leetcode.com/problems/two-sum/
 
+// func twoSum(nums []int, target int) []int {
+// 	var result []int
+// 	for i := 0; i < len(nums); i++ {
+// 		for j := i + 1; j < len(nums); j++ {
+// 			if nums[i]+nums[j] == target {
+// 				result = []int{i, j}
+// 				return result
+// 			}
+// 		}
+// 	}
+// 	return result
+// }
+
 func twoSum(nums []int, target int) []int {
-	var result []int
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				result = []int{i, j}
-				return result
-			}
+	mapper := map[int]int{}
+	for j, num := range nums {
+		complementary := target - num
+		if i, ok := mapper[num]; ok {
+			return []int{i, j}
 		}
+		mapper[complementary] = mapper[complementary] | j
 	}
-	return result
+	return []int{}
 }
 
 func main() {
-	nums := []int{1, 3, 4, 6, 2, 5}
-	target := 7
+	nums := []int{3, 3}
+	target := 6
 	fmt.Println(twoSum(nums, target))
 }
